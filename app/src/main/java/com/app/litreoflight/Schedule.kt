8e0a2584f32876760a711ade4b9c926e6d29da59
Leon <103996275+LeonPotgiter@users.noreply.github.com>
2024-08-06 09:37:51 +0200

    commit

---
+++ app/src/main/java/com/app/litreoflight/Schedule.kt
@@ -0,0 +1,31 @@
+package com.app.litreoflight
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Schedule : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_schedule)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnSchedule = findViewById<Button>(R.id.btnSchedule)
+        val list = findViewById<ListView>(R.id.listView)
+
+        btnSchedule.setOnClickListener {
+
+        }
+
+
+
+    }
+}
